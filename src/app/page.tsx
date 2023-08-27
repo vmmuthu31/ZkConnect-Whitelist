1bcdae85bdc5dc8b5fc470fb844dd4bbfaee7361
thirumurugan7 <thiru82003@gmail.com>
2023-08-27 08:55:46 +0530

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,57 @@
 import Image from 'next/image'
-
+import bg from "./bgvid.mp4";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+   <section className=' max-h-fit pb-[100px] relative'>
+     <video
+    className='absolute top-0 left-0 w-full h-full object-cover z-[-1]'
+    autoPlay
+    loop
+    muted
+  >
+    <source src={"./bgvid.mp4"} type="video/mp4" />
+    Your browser does not support the video tag.
+  </video>
+<div className='flex justify-center items-center'>
+<h1 className='text-white md:text-[75px] text-[50px] font-semibold pt-[30px]'>Zk Connect 🚀</h1>
+</div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+<div className='lg:mx-[300px] md:mx-[100px]  border-[10px] border-solid border-[#f5b8ff] rounded-[25px] bg-black bg-opacity-60'>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+<div className='md:p-[50px] p-[25px]'>
+  <h1 className='text-[#DB00FF] font-sans md:text-center  md:text-[42px] text-[30px] font-bold leading-normal line-clamp-3 '>
+  "Unlock Tech Talent with ZkConnect"
+  </h1>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+  <h1 className='text-white text-[23px] font-semibold leading-[29px] mt-[30px] text-center'>
+  Join Our Waiting List for Early Access
+  </h1>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+  <h1 className='text-white  font-sans text-[29px] font-normal leading-[38px] text-center mt-[60px]'>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+  Welcome to ZkConnect! Our AI-driven platform simplifies tech talent acquisition. Join our waiting list for early access and be part of the future of recruitment.
+  </h1>
+</div>
+
+<div className='flex justify-center items-center flex-col'>
+<div className='mb-[40px]'>
+  <input type="text" name="" id="" placeholder='Enter Your Name' className='bg-transparent rounded-md md:w-[450px] h-[62px] py-[15px] px-[15px] border placeholder:text-[#D2D2D2] placeholder:text-[23px] font-semibold' />
+</div>
+<div className='mb-[40px]'>
+  <input type="text" name="" id="" placeholder='Enter Your Phone No' className='bg-transparent rounded-md md:w-[450px] h-[62px] py-[15px] px-[15px] border placeholder:text-[#D2D2D2] placeholder:text-[23px] font-semibold' />
+</div>
+<div className='mb-[40px]'>
+  <input type="text" name="" id="" placeholder='Enter Your Email' className='bg-transparent rounded-md md:w-[450px] h-[62px] py-[15px] px-[15px] border placeholder:text-[#D2D2D2] placeholder:text-[23px] font-semibold' />
+</div>
+
+<button className='bg-[#D9D9D9] rounded-[16px] flex justify-center items-center px-[10px] py-[26px] mb-[20px]'> 
+
+<span className='text-black font-sans font-normal  leading-normal'>
+Join Waiting List
+</span>
+</button>
+</div>
+</div>
+   </section>
   )
 }
